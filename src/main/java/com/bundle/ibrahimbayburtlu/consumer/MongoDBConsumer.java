6cb733cf1c1522981a891991a306045f18bb36ee
ibrahim bayburtlu <ibrahimbayburtlu5gmail.com>
2023-10-30 00:10:08 +0300

    Bugs Fix

--- src/main/java/com/bundle/ibrahimbayburtlu/consumer/MongoDBConsumer.java
+++ src/main/java/com/bundle/ibrahimbayburtlu/consumer/MongoDBConsumer.java
@@ -6,8 +6,6 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class MongoDBConsumer {
 
